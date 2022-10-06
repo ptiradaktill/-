9c50b59ba93405bc85e18fe8fcf76ccb808da708
Алина Жидких <coo.alinazh@yandex.ru>
2022-10-06 16:58:31 +0300

    добавили папку гит коммит

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+void Number(int newNumber)
+{
+    if (newNumber < 1000 && newNumber > 99)
+    {
+        int result = newNumber/10;
+        Console.WriteLine(result%10);
+    }
+}
+
+Number(157);
---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+void number(int newNumber)
+{
+    if(newNumber > 99)
+    {
+        while(newNumber > 999)
+        {
+            newNumber = newNumber/10;
+        }
+        Console.WriteLine(newNumber%10);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+number(351);
